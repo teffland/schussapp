5aaa4310ee589f7d645a20df348d7f59ea7ede75
Tom Effland <tom.effland@gmail.com>
2014-10-20 12:21:41 -0400

    removed unnecessary stats folder

--- schussapp/stats/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-### Stats index ###
-def stats_home(request):
-    
-    return render(request, 'stats/stats_home.html',{
-                    'active':'stats'
-                  })
